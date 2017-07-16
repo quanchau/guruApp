e6401bfbdeba76fee0213f816dbcf2ed46198808
Anh Tran <trana1906@gmail.com>
2017-07-16 11:26:08 +0700

    - added signIn form and logo to loginView

--- src/Container/Login/LoginView.js
+++ src/Container/Login/LoginView.js
@@ -1,17 +1,39 @@
 import React, {Component} from 'react';
-import { View, StyleShee } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
 import {
   Button,
   Text,
+  Content,
+  Form,
+  Item,
+  Input,
+  Thumbnail,Container,
 } from 'native-base';
 
 export default class LoginView extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Button onPress={() => this.props.navigation.navigate('Main')}>
-          <Text>Go !</Text>
-        </Button>
+        <Container>
+        <Content contentContainerStyle={styles.formSignIn}>
+          <Form >
+            <Image
+            style={styles.logo}
+            source={require('./logoGuru.png')} />
+            <Item>
+              <Input placeholder="Username" />
+            </Item>
+            <Item last>
+              <Input placeholder="Password" />
+            </Item>
+            <Button
+              style={styles.button}
+              onPress={() => this.props.navigation.navigate('Main')}>
+              <Text>Sign In</Text>
+            </Button>
+          </Form>
+        </Content>
+        </Container>
       </View>
     );
   }
@@ -22,8 +44,21 @@ const styles = {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: 56,
     backgroundColor: '#ecf0f1',
+  },
+  formSignIn: {
+    paddingTop: 20,
+    justifyContent: 'center',
+  },
+  logo: {
+    height: 200,
+    width: 200,
+    marginTop: 10,
+    marginBottom: 50,
+  },
+  button: {
+    flex: 1,
+    marginTop: 20,
   },
   paragraph: {
     margin: 24,
